7cfe43883d4734bc604c0f4703187c95efe6a4e4
Andrea <acesparza05@gmail.com>
2022-07-12 16:24:47 -0500

    .gitignore added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Global Variables
 const catUrl = 'https://http.cat/'
-
+const answerChoice = document.getElementsByClassName("answer-choice")
+const question = document.getElementsByClassName("question")
 
 
