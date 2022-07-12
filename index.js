224f793cc751175f1d646f5ebb65b51189546692
Andrea <acesparza05@gmail.com>
2022-07-12 17:03:04 -0500

    working on event listeners

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 // Global Variables
-const catUrl = 'https://http.cat/'
+const catUrl = 'https://api.thecatapi.com/v1/images/search'
 const answerChoice = document.getElementsByClassName("answer-choice")
 const question = document.getElementsByClassName("question")
 
+fetch(catUrl)
+.then(res => res.json())
+.then(cat => console.log(cat))
+
+
+
+const handleMouse = () => {
+    
+}
+
+
+function handleClick(e) {
+    Array.prototype.forEach.call(answerChoice, function(el) {
+        console.log(el.tagName);
+    });
+}
+handleClick()
+
+
+
+let form = document.querySelector('form')
 
