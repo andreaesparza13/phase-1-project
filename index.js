e82aec364fe9d801b9434242a67a7b85bccb8b6b
Andrea <acesparza05@gmail.com>
2022-07-13 13:42:19 -0500

    almost done

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 // Global Variables
 const catUrl = 'https://api.thecatapi.com/v1/images/search'
-const answerChoice = document.getElementsByClassName("answer-choice")
+const answerChoice = document.querySelectorAll('.answer-choice')
 const question = document.getElementsByClassName("question")
 
-fetch(catUrl)
-.then(res => res.json())
-.then(cat => console.log(cat))
+function displayCat() {
+    fetch(catUrl)
+    .then(res => res.json())
+    .then(cat => console.log(cat))
+}
 
 
 
 const handleMouse = () => {
-    
+    answerChoice.forEach(choice => {
+        choice.addEventListener('mouseenter', (e) => {
+            e.target.style.fontSize = '30px'
+        })
+        choice.addEventListener('mouseleave', (e) => {
+            e.target.style.fontSize = '20px'
+        })
+    }) 
 }
 
 
-function handleClick(e) {
-    Array.prototype.forEach.call(answerChoice, function(el) {
-        console.log(el.tagName);
+function handleClick() {
+    let i = 0
+    answerChoice.forEach(choice => {
+        choice.addEventListener('click', e => {
+            if (i % 2 == 0) {
+                e.target.style.backgroundColor = "lime"
+            } else {
+                e.target.style.backgroundColor = "white"
+            }
+            i++
+
+        })
     });
 }
+
+
+function handleSubmit(){
+    const form = document.querySelector("#name-box")
+    const btn = document.querySelector('#submit-form')
+    btn.textContent = 'Submit'
+    form.addEventListener('submit', e => {
+        // e.preventDefault()
+        const userInput = e.target.name.value
+        alert (`Congratulations, ${userInput}, you are a cat person!`)
+        form.reset()
+    })
+}
+
+// Function Invocations
+handleMouse()
 handleClick()
-
-
-
-let form = document.querySelector('form')
-
+handleSubmit()
